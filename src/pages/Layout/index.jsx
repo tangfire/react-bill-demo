d631caf5d136a51006264547865422bbcc22268e
tangfire <52628216@qq.com>
2025-11-17 22:52:10 +0800

    Day4 TabBar功能实现

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,21 +1,67 @@
-import {Outlet} from "react-router-dom";
-import {Button} from "antd-mobile";
+import {Outlet, useNavigate} from "react-router-dom";
 import {useEffect} from "react";
 import {useDispatch} from "react-redux";
 import {getBillList} from "@/store/modules/billStore.js";
+import React from 'react'
+import { TabBar } from 'antd-mobile'
+import './index.scss'
+import {
+
+    AddCircleOutline,
+    HandPayCircleOutline,
+    ShopbagOutline,
+} from 'antd-mobile-icons'
+
+const tabs = [
+    {
+        key: 'month',
+        title: '月度账单',
+        icon: <HandPayCircleOutline />,
+    },
+    {
+        key: 'new',
+        title: '记账',
+        icon: <AddCircleOutline />,
+    },
+    {
+        key: 'year',
+        title: '年度账单',
+        icon: <ShopbagOutline />,
+    },
+
+]
+
+
 
 const Layout = () => {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(getBillList());
     }, [dispatch]);
+
+    // 切换菜单跳转路由
+    const navigate = useNavigate();
+    const switchRoute = (path) => {
+        console.log(path);
+        navigate(path);
+
+    }
     return (
 
-        <div>
+        <div className="layout">
 
-            <Outlet></Outlet>
-            我是layout
-            <Button color="primary">测试全局</Button>
+            <div className="container">
+                <Outlet></Outlet>
+            </div>
+
+
+            <div className="footer">
+                <TabBar onChange={switchRoute}>
+                    {tabs.map(item => (
+                        <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+                    ))}
+                </TabBar>
+            </div>
 
         </div>
     )

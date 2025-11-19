4097c016af373189016864071d21b065412bb6e4
tangfire <52628216@qq.com>
2025-11-19 22:30:13 +0800

    Day5 月度账单-单日统计列表渲染

--- src/pages/Month/components/DayBill/index.jsx
+++ src/pages/Month/components/DayBill/index.jsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames'
 import './index.scss'
 import {useMemo} from "react";
+import {billTypeToName} from "@/contants/index.js";
 
 const DailyBill = ({date,billList}) => {
 
@@ -38,6 +39,22 @@ const DailyBill = ({date,billList}) => {
                         <span className="type">结余</span>
                     </div>
                 </div>
+            </div>
+            <div className="billList">
+                {
+                    billList.map(item => {
+                        return (
+                            <div className="bill" key={item.id}>
+                                <div className="detail">
+                                    <div className="billType">{billTypeToName[item.useFor]}</div>
+                                </div>
+                                <div className={classNames('money',item.type)}>
+                                    {item.money.toFixed(2)}
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
 
         </div>
